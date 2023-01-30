a1128e3c0c682d0c87de87fe9f2fd2d0b1d1cb86
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-01-30 12:09:07 +0100

    create the project folder

---
+++ MonSalonline/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+// import Tasks from './components/Tasks.vue'; 
+export default {
+  name :"app",
+  // components : {Tasks},
+
+  data() {
+    return {
+      title: "Tasks Apps",
+      
+    };
+  },
+
+  methods: {
+    
+  }
+};
+</script>
+
+<template>
+  
+ <div id="app">
+  <h1>Hello Jelouani Abdelghani !!</h1>
+  <h3>{{ title }}</h3>
+ </div>
+ 
+</template>
+
+<style>
+/* .card{
+  padding: 20px;
+  background: #eee;
+} */
+</style>
