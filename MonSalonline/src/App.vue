9c469660aa4a634540d201df899435ec0d0bcbb3
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-02 08:52:58 +0100

    modification

--- MonSalonline/src/App.vue
+++ MonSalonline/src/App.vue
@@ -1,31 +1,38 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
-// import Tasks from './components/Tasks.vue'; 
-export default {
-  name :"app",
-  // components : {Tasks},
 
-  data() {
-    return {
-      title: "Tasks Apps",
-      
-    };
-  },
 
-  methods: {
-    
-  }
-};
+
 </script>
 
 <template>
-  
- <div id="app">
-  <h1>Hello Jelouani Abdelghani !!</h1>
-  <h3>{{ title }}</h3>
- </div>
- 
+  <div class="m-4">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a href="#" class="navbar-brand"><img width="50px" height="50px" style="border-radius: 50%;" src="../public/ModernCoupe.png"></a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+          <div class="navbar-nav">
+            <a href="#" class="nav-item nav-link active">Accueil</a>
+            <a href="#" class="nav-item nav-link">Services</a>
+          </div>
+          <form class="d-flex">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search">
+              <button type="button" class="btn btn-secondary"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+          </form>
+          <div class="navbar-nav">
+            <a href="#" class="nav-item nav-link">Login</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
 </template>
 
 <style>
